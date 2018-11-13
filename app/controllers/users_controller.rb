d92c24690f6c39e0c0ae6e2e987cc90ef21d2121
wojtek <wojtek.kostanski@gmail.com>
2018-11-14 00:30:33 +0100

    add validators

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,22 +4,19 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
-  
+
   def new; end
 
   def create
     @user = User.new
-    validation = UserValidator::UserSchema.with(record: @user).call(params.permit!.to_h)
+    validation = UserValidator::UserSchema.with(record: @user)
+                                          .call(params.permit!.to_h)
     if validation.success?
       @user.attributes = validation.output[:user]
       @user.save
-      redirect_to new_user_path, notice: 'Form send!'
+      redirect_to users_path, notice: 'Form send!'
     else
-      p 'error'
-      set_user
-      respond_to do |format|
-        format.json { render json: validation.errors }
-      end
+      redirect_to new_user_path, alert: validation.errors.to_s
     end
   end
 
@@ -32,13 +29,4 @@ class UsersController < ApplicationController
     @user.company.address = Address.new
     @user
   end
-  # def user_params
-  #   params.require(:user).permit(:first_name,
-  #                               :last_name,
-  #                               :email_address,
-  #                               :date_of_birth,
-  #                               :phone_number,
-  #                               company_attributes: [:id, :name, address_attributes: [:id, :street, :city, :zip_code, :country]],
-  #                               address_attributes: [:id, :street, :city, :zip_code, :country])
-  # end
 end
--- app/validators/user_validator.rb
+++ app/validators/user_validator.rb
@@ -1,17 +1,55 @@
 class UserValidator
+  include ValidatorRegex
   UserSchema = Dry::Validation.Params do
+    configure do
+      config.messages_file =
+        File.join(Rails.root, 'config', 'locales', 'validation_errors.en.yml')
+
+      def country?(value)
+        COUNTRY_LIST.include? value.to_s
+      end
+
+      def email?(value)
+        !EMAIL_REGEX.match(value).nil?
+      end
+
+      def correct_date?(value)
+        if !DATE_REGEX.match(value).nil?
+          Date.strptime(value, '%d-%m-%Y') < Date.today
+        else
+          false
+        end
+      end
+
+      def phone?(value)
+        !PHONE_REGEX.match(value).nil?
+      end
+
+      def zip_code?(value)
+        !ZIP_CODE_REGEX.match(value).nil?
+      end
+    end
     required(:user).schema do
-      required(:first_name).filled
+      required(:first_name) { filled? && max_size?(100) }
+      required(:last_name) { filled? && max_size?(100) }
+      required(:email_address) { filled? && email? }
+      required(:date_of_birth).maybe(:correct_date?)
+      required(:phone_number).maybe(:phone?)
+      required(:address_attributes).schema do
+        required(:street) { filled? }
+        required(:city) { filled? }
+        required(:zip_code) { filled? && zip_code? }
+        required(:country) { filled? && country? }
+      end
+      required(:company_attributes).schema do
+        required(:name) { max_size?(200) }
+        required(:address_attributes).schema do
+          required(:street).maybe(:str?)
+          required(:city).maybe(:str?)
+          required(:zip_code).maybe(:zip_code?)
+          required(:country).maybe(:country?)
+        end
+      end
     end
   end
 end
-
-  # def user_params
-  #   params.require(:user).permit(:first_name,
-  #                               :last_name,
-  #                               :email_address,
-  #                               :date_of_birth,
-  #                               :phone_number,
-  #                               company_attributes: [:id, :name, address_attributes: [:id, :street, :city, :zip_code, :country]],
-  #                               address_attributes: [:id, :street, :city, :zip_code, :country])
-  # end
