5d616407c5bfc07254c5d22ce3dca802f52e4421
wojtek <wojtek.kostanski@gmail.com>
2018-11-13 22:39:24 +0100

    add users index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,37 @@
 class UsersController < ApplicationController
-  def new
-    @user = User.new
-    @user.address = Address.new
-    @user.company = Company.new
-    @user.company.address = Address.new
+  before_action :set_user, only: [:new]
+
+  def index
+    @users = User.all
   end
+  
+  def new; end
 
   def create
     @user = User.new
-    p '+'*10
-    p params.permit!.to_h
-    
     validation = UserValidator::UserSchema.with(record: @user).call(params.permit!.to_h)
-    p validation
     if validation.success?
       @user.attributes = validation.output[:user]
       @user.save
       redirect_to new_user_path, notice: 'Form send!'
     else
-      redirect_to new_user_path, warning: "#{validation.errors}"
+      p 'error'
+      set_user
+      respond_to do |format|
+        format.json { render json: validation.errors }
+      end
     end
   end
 
+  private
+
+  def set_user
+    @user = User.new
+    @user.address = Address.new
+    @user.company = Company.new
+    @user.company.address = Address.new
+    @user
+  end
   # def user_params
   #   params.require(:user).permit(:first_name,
   #                               :last_name,
